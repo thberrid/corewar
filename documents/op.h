8548bbddbea95ffa601dbe0b67ac4ca71be52210
Arthur Baurens <abaurens@student.42.fr>
2019-10-14 17:16:38 +0200

    normed op.h

--- documents/op.h
+++ documents/op.h
@@ -3,97 +3,86 @@
 /*                                                        :::      ::::::::   */
 /*   op.h                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: zaz <zaz@student.42.fr>                    +#+  +:+       +#+        */
+/*   By: baurens <baurens@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2013/10/04 11:33:27 by zaz               #+#    #+#             */
-/*   Updated: 2019/10/14 15:28:18 by baurens          ###   ########.fr       */
+/*   Created: 2019/10/14 15:51:41 by baurens           #+#    #+#             */
+/*   Updated: 2019/10/14 15:59:37 by baurens          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/*
-** Toutes les tailles sont en octets.
-** On part du principe qu'un int fait 32 bits. Est-ce vrai chez vous ?
-*/
+#ifndef OP_H
+# define OP_H
 
-#include <inttypes.h>
+# include <inttypes.h>
 
-#define IND_SIZE				2
-#define REG_SIZE				4
-#define DIR_SIZE				REG_SIZE
-typedef uint16_t				t_ind;
-typedef uint32_t				t_reg;
-typedef t_reg					t_dir;
+# define REG_CODE			1
+# define DIR_CODE			2
+# define IND_CODE			3
 
-typedef uint8_t					t_byte;
+# define MAX_ARGS_NUMBER	4
+# define MAX_PLAYERS		4
+# define MEM_SIZE			(4*1024)
+# define IDX_MOD			(MEM_SIZE / 8)
+# define CHAMP_MAX_SIZE		(MEM_SIZE / 6)
 
+# define COMMENT_CHAR		'#'
+# define LABEL_CHAR			':'
+# define DIRECT_CHAR		'%'
+# define SEPARATOR_CHAR		','
 
-# define REG_CODE				1
-# define DIR_CODE				2
-# define IND_CODE				3
+# define LABEL_CHARS		"abcdefghijklmnopqrstuvwxyz_0123456789"
 
+# define NAME_CMD_STRING	".name"
+# define COMMENT_CMD_STRING	".comment"
 
-#define MAX_ARGS_NUMBER			4
-#define MAX_PLAYERS				4
-#define MEM_SIZE				(4*1024)
-#define IDX_MOD					(MEM_SIZE / 8)
-#define CHAMP_MAX_SIZE			(MEM_SIZE / 6)
+# define REG_NUMBER			16
 
-#define COMMENT_CHAR			'#'
-#define LABEL_CHAR				':'
-#define DIRECT_CHAR				'%'
-#define SEPARATOR_CHAR			','
+# define CYCLE_TO_DIE		1536
+# define CYCLE_DELTA		50
+# define NBR_LIVE			21
+# define MAX_CHECKS			10
 
-#define LABEL_CHARS				"abcdefghijklmnopqrstuvwxyz_0123456789"
+# define T_REG				1
+# define T_DIR				2
+# define T_IND				4
+# define T_LAB				8
 
-#define NAME_CMD_STRING			".name"
-#define COMMENT_CMD_STRING		".comment"
+# define PROG_NAME_LENGTH	(128)
+# define COMMENT_LENGTH		(2048)
+# define COREWAR_EXEC_MAGIC	0xea83f3
 
-#define REG_NUMBER				16
+# define IND_SIZE			2
+# define REG_SIZE			4
+# define DIR_SIZE			REG_SIZE
+typedef uint16_t			t_ind;
+typedef uint32_t			t_reg;
+typedef t_reg				t_dir;
+typedef t_ind				t_hdir;
+typedef uint8_t				t_byte;
+typedef char				t_arg_type;
 
-#define CYCLE_TO_DIE			1536
-#define CYCLE_DELTA				50
-#define NBR_LIVE				21
-#define MAX_CHECKS				10
-
-/*
-**
-*/
-
-typedef char	t_arg_type;
-
-#define T_REG					1
-#define T_DIR					2
-#define T_IND					4
-#define T_LAB					8
-
-/*
-**
-*/
-
-# define PROG_NAME_LENGTH		(128)
-# define COMMENT_LENGTH			(2048)
-# define COREWAR_EXEC_MAGIC		0xea83f3
-
-typedef struct		header_s
+typedef struct	s_header
 {
-  unsigned int		magic;
-  char				prog_name[PROG_NAME_LENGTH + 1];
-  unsigned int		prog_size;
-  char				comment[COMMENT_LENGTH + 1];
-}					header_t;
+  uint32_t		magic;
+  char			prog_name[PROG_NAME_LENGTH + 1];
+  uint32_t		prog_size;
+  char			comment[COMMENT_LENGTH + 1];
+}				t_header;
 
-typedef struct		s_op
+typedef struct	s_op
 {
-	char			*name;
-	int				arg_cnt;
-	char			args[4];
-	int				id;
-	int				cost;
-	char			*description;
-	char			ocp;
-	char			hdir;
-	char			(*func)(void)
-}					t_op;
+	char		*name;
+	int			arg_cnt;
+	char		args[4];
+	int			id;
+	int			cost;
+	char		*desc;
+	char		ocp;
+	char		hdir;
+	char		(*fnc)(void);
+}				t_op;
 /*
 **	(t_proc *process, char *mem);
 */
+
+#endif
