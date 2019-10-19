6cbf5d534b00738239081359afac3a27a6a93db0
Thibault BERRIDO <thberrid@e1r2p14.42.fr>
2019-10-20 00:20:24 +0200

    clean valgrind errors

---
+++ srcs/asm/instruct_free.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   instruct_free.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: thberrid <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/19 05:10:47 by thberrid          #+#    #+#             */
+/*   Updated: 2019/10/19 05:20:55 by thberrid         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <asm.h>
+
+void	clean_struct(t_instruct_head *head)
+{
+	size_t		i;
+	t_instruct	*tmp;
+	t_instruct	*next;
+
+	i = 0;
+	tmp = head->head;
+	while (i < head->slen)
+	{
+		next = tmp->next;
+		ft_memdel((void **)&(tmp->label));
+		ft_memdel((void **)&tmp);
+		tmp = next;
+		i++;
+	}
+}
