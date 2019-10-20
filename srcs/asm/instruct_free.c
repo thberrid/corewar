f042cc986e04ae380d9862ef06f6e39af60d45f4
Thibault BERRIDO <thberrid@e1r2p14.42.fr>
2019-10-20 02:58:25 +0200

    same same, just renamed

--- srcs/asm/instruct_free.c
+++ srcs/asm/instruct_free.c
@@ -6,13 +6,13 @@
 /*   By: thberrid <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/19 05:10:47 by thberrid          #+#    #+#             */
-/*   Updated: 2019/10/19 05:20:55 by thberrid         ###   ########.fr       */
+/*   Updated: 2019/10/20 02:42:12 by thberrid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <asm.h>
 
-void	clean_struct(t_instruct_head *head)
+void	instruct_free(t_instruct_head *head)
 {
 	size_t		i;
 	t_instruct	*tmp;
