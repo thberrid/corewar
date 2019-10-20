bbf3499d10e38428cfd9ad936e7956442ec095f6
Thibault BERRIDO <thberrid@e1r2p14.42.fr>
2019-10-20 07:35:15 +0200

    new features : select FD, and can print in reverse

--- srcs/asm/ft_bprint.c
+++ srcs/asm/ft_bprint.c
@@ -6,27 +6,52 @@
 /*   By: thberrid <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/20 02:29:35 by thberrid          #+#    #+#             */
-/*   Updated: 2019/10/20 02:34:10 by thberrid         ###   ########.fr       */
+/*   Updated: 2019/10/20 07:32:15 by thberrid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ftlib.h"
 
-void	ft_bprint(void *ptr, int nbits)
+void	ft_bprint_fd(void *ptr, int octet_len, int fd)
 {
 	int				i;
+	int				j;
 	unsigned char 	value;
 
 	i = 0;
-	value = *((unsigned char *)ptr);
-	while (i < nbits)
+	while (i < octet_len)
 	{
-		ft_putnbr(((value & 1) ? 1 : 0));
-		value >>= 1;
-		i += 1;
-		if (!(i % 4))
-			ft_putchar(' ');
+		value = ((unsigned char *)ptr)[i];
+		j = 0;
+		while (j < 8)
+		{
+			ft_putnbr_fd(((value & 1) ? 1 : 0), fd);
+			j++;
+			if (!(j % 4))
+				ft_putchar_fd(' ', fd);
+			value >>= 1;
+		}
+		ft_putchar_fd(' ', fd);
+		i++;
 	}
-	ft_putchar('\n');
 }
 
+void	ft_bprint_fd_rev(void *ptr, int octet_len, int fd)
+{
+	int				i;
+	int				j;
+	unsigned char 	value;
+
+	i = 0;
+	while (i < octet_len)
+	{
+		value = ((unsigned char *)ptr)[octet_len - i - 1];
+		j = 8;
+		while (j)
+		{
+			ft_putnbr_fd((((value >> (j - 1)) & 1) ? 1 : 0), fd);
+			j--;
+		}
+		i++;
+	}
+}
