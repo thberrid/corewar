8fe7ab2552fd1d4f4bc622c041b696e5023bbc0d
Arthur Baurens <elementscitiessuport@gmail.com>
2019-10-21 12:05:16 +0200

    added attribute unused to endianes function to avoid compilation warning

--- includes/endianes.h
+++ includes/endianes.h
@@ -6,7 +6,7 @@
 /*   By: abaurens <abaurens@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/15 07:26:33 by abaurens          #+#    #+#             */
-/*   Updated: 2019/10/20 21:00:04 by baurens          ###   ########.fr       */
+/*   Updated: 2019/10/21 12:03:00 by abaurens         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,12 @@
 
 # define DEPRECATION_MSG	"use 'system_to_bin' or 'bin_to_system' instead"
 
-void	swap_endianes(void *const val,
-			size_t ln) __attribute__((deprecated(DEPRECATION_MSG)));
+void		swap_endianes(void *const val,
+				size_t ln) __attribute__((deprecated(DEPRECATION_MSG)));
 static void	system_to_bin(void *const val,
-			size_t ln) __attribute__((weakref("swap_endianes")));
+				size_t ln) __attribute__((unused,weakref("swap_endianes")));
 static void	bin_to_system(void *const val,
-			size_t ln) __attribute__((weakref("swap_endianes")));
+				size_t ln) __attribute__((unused,weakref("swap_endianes")));
 
 enum	e_endianes
 {
