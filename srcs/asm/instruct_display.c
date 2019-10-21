f53607a3202de825d676384f54599e5751a96187
Thibault BERRIDO <thberrid@e1r2p8.42.fr>
2019-10-21 07:02:16 +0200

    update to handle char *param_str[3]

--- srcs/asm/instruct_display.c
+++ srcs/asm/instruct_display.c
@@ -6,7 +6,7 @@
 /*   By: thberrid <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/20 02:52:22 by thberrid          #+#    #+#             */
-/*   Updated: 2019/10/20 07:29:11 by thberrid         ###   ########.fr       */
+/*   Updated: 2019/10/21 06:55:18 by thberrid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,19 @@
 
 void	instruct_display(t_instruct *this)
 {
-	ft_printf("Label : %s\n", this->label);
-	ft_printf("ID : %d\n", this->id);
-	ft_printf("OCP : ");
+	int		i;
+
+	ft_printf("Label -> %s\n", this->label);
+	ft_printf("ID -> %d\n", this->id);
+	ft_printf("OCP -> ");
 	ft_bprint_fd_rev(&(this->ocp), sizeof(this->ocp), 1);
+	i = 0;
 	ft_putchar('\n');
+	while (this->params_str[i])
+	{
+		ft_printf("P%d -> %s\n", i + 1, this->params_str[i]);
+		i += 1;
+	}
 }
 
 void	instruct_display_all(t_instruct_head *head)
