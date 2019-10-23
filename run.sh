6d990bcfa56019d1513afe334e269a039008f691
Arthur Baurens <elementscitiessuport@gmail.com>
2019-10-23 20:06:20 +0200

    shell script

--- run.sh
+++ run.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+TESTER=./tester
+YOUR_VM=./corewar
+ZAZ_VM=./documents/vm_champs/corewar
+
+# The option to make your vm dump the memory as zaz's one do it (same format).
+DUMP_OPT="-zdump"
+
+YOUR_OUT_FILE=yout.dump
+ZAZ_OUT_FILE=zout.dump
+
 print_usage()
 {
 	echo "Usage:"
@@ -8,7 +18,49 @@ print_usage()
 
 compare_corewar()
 {
-	
+	echo "$YOUR_VM $DUMP_OPT $1 $2 > $YOUR_OUT_FILE"
+	echo "$ZAZ_VM -d $1 $2 > $ZAZ_OUT_FILE"
+	echo
+}
+
+get_op_cost()
+{
+	cat includes/op.h | grep "{\"$1\", " | sed -E "s:(T_DIR|T_REG|T_IND)::g"| sed -E "s:\{[ |,]+\}::g" | sed -E "s:\{\".*\", [0-9]+, , [0-9]+, ::g"
+#	if [ $1 = "live" ]; then
+#		echo "10"
+#	elif [ $1 = "ld" ]; then
+#		echo "5"
+#	elif [ $1 = "st" ]; then
+#		echo "5"
+#	elif [ $1 = "add" ]; then
+#		echo "10"
+#	elif [ $1 = "sub" ]; then
+#		echo "10"
+#	elif [ $1 = "and" ]; then
+#		echo "6"
+#	elif [ $1 = "or" ]; then
+#		echo "6"
+#	elif [ $1 = "xor" ]; then
+#		echo "6"
+#	elif [ $1 = "zjmp" ]; then
+#		echo "20"
+#	elif [ $1 = "ldi" ]; then
+#		echo "25"
+#	elif [ $1 = "sti" ]; then
+#		echo "25"
+#	elif [ $1 = "fork" ]; then
+#		echo "800"
+#	elif [ $1 = "lld" ]; then
+#		echo "10"
+#	elif [ $1 = "lldi" ]; then
+#		echo "50"
+#	elif [ $1 = "lfork" ]; then
+#		echo "1000"
+#	elif [ $1 = "aff" ]; then
+#		echo "2"
+#	else
+#		echo "0"
+#	fi
 }
 
 if [[ -n $1 ]]; then
@@ -21,7 +73,8 @@ if [[ -n $1 ]]; then
 	fi
 	for file in `find tests/$1 -type f -name "*.cor"`
 	do
-		echo "$file"
+		get_op_cost $1
+		#compare_corewar `get_op_cost $1` $file
 	done
 else
 	print_usage
