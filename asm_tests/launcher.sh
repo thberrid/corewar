5cec9f025b420e138cce5cfc634207e1d5329258
Thibault BERRIDO <thberrid@e1r1p12.42.fr>
2019-10-30 06:48:24 +0100

    tests are now split in files.

--- asm_tests/launcher.sh
+++ asm_tests/launcher.sh
@@ -7,79 +7,34 @@ ASM_ORIGINAL=../documents/vm_champs/asm
 ASM_NEW=../documents/vm_champs/asm
 #ASM_NEW=../asm
 
+declare -i INDEX=1
+declare -i SUCCESS
+declare -l ERROR_MSG
 
 # FNS
 
 print_result ()
 {
-	if [ $3 ]; then
+	if [ $3 -gt 0 ]; then
 		echo -en "\e[1;32m$1 \e[0m"
 	else
 		echo -en "\e[1;31m$1 \e[0m"
 	fi
-	echo -e $2
-	echo "	$4"
+	echo -e $2 $4
 }
 
 
 # clean previous tests trace
 
-rm tmp/*.cor
-rm inputs/*.cor
+rm tmp/*.cor inputs/*.cor
 clear
 
-# test differents .s cases 
-# compile then diff all .s files in ./inputs/
-
-echo "---\n01 diff of .cor files\n"
-declare -i INDEX=1
-declare -i SUCCESS
-declare -l ERROR_MSG
-for filename in inputs/*.s; do
-	SUCCESS=0
-	ERROR_MSG=""
-    [ -e "$filename" ] || continue
-	$ASM_ORIGINAL $filename > /dev/null
-	BTCODE_NAME=`echo $filename | cut -d / -f 2 | cut -d . -f 1`
-	COR_FILE="inputs/$BTCODE_NAME.cor"
-	if [ -e $COR_FILE ]; then
-		COR_ORIGINAL="tmp/$BTCODE_NAME.cor"
-		mv $COR_FILE $COR_ORIGINAL
-		$ASM_NEW $filename > /dev/null
-		COR_NEW=`echo $filename | cut -d . -f 1`".cor"
-		if [ -e $COR_NEW ]; then
-			DIFF=`diff $COR_NEW $COR_ORIGINAL`
-			if [ "$DIFF" != "" ]; then
-				SUCCESS=0
-				ERROR_MSG="diff delta between the .cor files"
-			else
-				SUCCESS=1
-			fi
-		else
-			SUCCESS=0
-			ERROR_MSG="the .asm didn't compile"
-		fi
-	else	# else : the original ASM didn't compile
-		$ASM_NEW $filename > /dev/null
-		COR_NEW=`echo $filename | cut -d . -f 1`".cor"
-		if [ -e $COR_NEW ]; then
-			SUCCESS=0
-			ERROR_MSG="the .asm shouldn't had compile"
-		else
-			SUCCESS=1
-		fi
-	fi
-	print_result $INDEX $BTCODE_NAME $SUCCESS $ERROR_MSG
-	let "INDEX+=1"
-done
+# launch tests
 
 echo ""
-
-echo "02 tests of wrong input cases"
-# test wrong inputs
-#	dev / null, dev / random, etc
-#	not a .s file
-#	unexistent filename
-#	empty argument
-# 	several filename
-echo "---"
+for test_script in srcs/*; do
+	echo "> "`echo $test_script | cut -d / -f 2`"\n"
+	source $test_script
+	echo "\n"
+done
+echo ""
