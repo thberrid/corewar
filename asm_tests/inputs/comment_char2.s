c8dd933b48e280844b37160f2bc002140f64465d
Thibault BERRIDO <thberrid@e3r2p10.42.fr>
2019-11-09 06:19:30 +0100

    add new tests

---
+++ asm_tests/inputs/comment_char2.s
@@ -0,0 +1,10 @@
+#comment
+.name "on dit pas hein"
+#comment
+.comment "putain mais lol mec"
+
+	#comment
+live %1	#hein
+ #comment live %1
+ 	live %1
+
---
+++ asm_tests/inputs/comment_char3.s
@@ -0,0 +1,9 @@
+.name "on dit pas hein"	#comment
+.comment "putain mais lol mec"
+
+#comment
+	#comment
+live %1	#hein
+ #comment live %1
+ 	live %1
+
