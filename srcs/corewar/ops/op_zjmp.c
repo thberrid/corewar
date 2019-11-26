42efdf02c218bed236cd675357ddb17d739e501e
Arthur Baurens <abaurens@student.42.fr>
2019-11-26 16:29:32 +0100

    removed comment

--- srcs/corewar/ops/op_zjmp.c
+++ srcs/corewar/ops/op_zjmp.c
@@ -6,7 +6,7 @@
 /*   By: baurens <baurens@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/15 10:05:20 by abaurens          #+#    #+#             */
-/*   Updated: 2019/11/26 03:15:39 by baurens          ###   ########.fr       */
+/*   Updated: 2019/11/26 16:28:16 by baurens          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,8 +46,6 @@ char		op_zjmp(t_vm *vm, t_proc *proc)
 		pc %= MEM_SIZE;
 		if (pc < 0)
 			pc += MEM_SIZE;
-		/*if ((int16_t)(proc->pc = (proc->pc + (val % IDX_MOD)) % MEM_SIZE) < 0)
-			proc->pc = MEM_SIZE - proc->pc;*/
 		proc->pc = pc;
 	}
 	return (proc->carry);
