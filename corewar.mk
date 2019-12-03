c2f860e8be09c944d931fac4bbb84fb0dedc4d04
Arthur Baurens <elementscitiessuport@gmail.com>
2019-12-03 09:48:01 +0100

    splited tester and vm makefiles

--- corewar.mk
+++ corewar.mk
@@ -68,43 +68,4 @@ SRC_COR	:=	$(PARSER)	\
 			cmd_parse.c	\
 			$(GRAPHIC)
 
-#
-#	vm unit tests
-#
-TESTER	:=	tester
-
-SPEC	:=	\
-			or.c	\
-			st.c	\
-			ld.c	\
-			add.c	\
-			sub.c	\
-			and.c	\
-			xor.c	\
-			aff.c	\
-			sti.c	\
-			ldi.c	\
-			lld.c	\
-			lldi.c	\
-			zjmp.c	\
-			live.c	\
-			fork.c	\
-			lfork.c
-SPEC	:=	$(addprefix specific_tests/,$(SPEC))
-
-SRC_TST	:=	\
-			$(SPEC)		\
-			main.c		\
-			random.c	\
-			file_gen.c
-SRC_TST	:=	$(addprefix $(TESTER)/,$(SRC_TST)) $(SRC_COM)
-
-OBJ_TST	:=	$(addprefix $(OBJD)/,$(SRC_TST:.c=.o))
-SRC_TST	:=	$(addprefix $(SRCD)/,$(SRC_TST))
-DEP_TST	:=	$(OBJ_TST:.o=.d)
-
--include $(DEP_TST)
-
-$(TESTER):	CFLAGS += -I./includes/tester -DVM_TEST
-$(TESTER): $(OBJ_TST)
-	$(LINKER) $(TESTER) $(OBJ_TST) $(LDFLAGS)
+-include vm_tester.mk
