095a7beb73267384470e7baffaa266f518b206c9
Arthur Baurens <elementscitiessuport@gmail.com>
2019-12-03 09:03:04 +0100

    removed graphic part from corewar.mk

--- corewar.mk
+++ corewar.mk
@@ -44,66 +44,11 @@ PARSER	:=	\
 PARSER	:=	$(addprefix parser/,$(PARSER))
 
 #
-#	REMOVE THIS BEFORE TURN-IN
-#
-override ZAZ	?= TRUE
-#override G		?= TRUE
-
-#
 #	VM compilation specific flags
 #
 override ZAZ ?= FALSE
 ifeq ($(ZAZ),TRUE)
 override ZAZ_FLAG	:= -DZAZ
-endif
-
-#
-#	Graphic (C++) part
-#
-ifdef G
-ifeq ($(shell uname), Darwin)
-
-GL	:=	-framework OpenGL
-$(COR):	CPPFLAGS	+= -I ~/.brew/include
-$(COR):	LDFLAGS +=	-L ~/.brew/lib
-
-else
-
-GL	:=	-lGL
-
-endif
-
-override GFLAG := -DGRAPHIC=1
-
-CPPFLAGS	:= -I./includes -MMD -MP -W -Wall -Wextra -Werror -std=c++14
-CPPFLAGS	+= -I./$(dir $(LIB))includes $(GFLAG) -O3 -I./includes/corewar
-CPPFLAGS	+= $(shell sdl2-config --cflags) -I./includes/graphic
-
-$(COR):	CFLAGS += $(GFLAG)
-$(COR):	LDFLAGS += $(shell sdl2-config --libs) -lSDL2_image $(GL) -lGLEW
-
-CLASS	:=	\
-			animation	\
-			transform	\
-			texture		\
-			skybox		\
-			shader		\
-			window		\
-			camera		\
-			chunk		\
-			cube
-CLASS	:=	$(addsuffix .cpp, $(CLASS))
-
-GRAPHIC	:= \
-		$(CLASS)		\
-		graphic_loop.cpp
-GRAPHIC	:= $(addprefix graphic/,$(GRAPHIC))
-override LINKER		:=	g++ -o
-
-$(OBJD)/%.o:	$(SRCD)/%.cpp
-	@mkdir -p $(dir $@)
-	g++ $(CPPFLAGS) -o $@ -c $<
-
 endif
 
 $(COR):	CFLAGS += -march=native -O3 -I./includes/corewar -ansi -pedantic -DVM $(ZAZ_FLAG)
