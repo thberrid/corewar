32a2e1360838d2d90a9e5525e2ac5b79cb31efb7
Arthur Baurens <elementscitiessuport@gmail.com>
2019-12-03 09:20:42 +0100

    added a little easter egg

--- includes/corewar/process.h
+++ includes/corewar/process.h
@@ -6,7 +6,7 @@
 /*   By: abaurens <abaurens@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/21 12:57:27 by abaurens          #+#    #+#             */
-/*   Updated: 2019/12/02 13:01:32 by abaurens         ###   ########.fr       */
+/*   Updated: 2019/12/03 09:20:14 by abaurens         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,5 +47,53 @@ t_proc			*kill_process(t_proc *proc);
 void			clear_procs(void);
 
 extern t_lst	g_procs;
+
+/*
+**	préparation pour gateau au chocolat de 500g:
+**	prévoire un glassage aux noix de pécan
+**	100 mL d'huile végétale
+**	quatres gros oeufs
+**	100g de pépites de chocolat
+**	200g de beure ou de margarine
+**	300g de sucre semoule
+**	200g de farine
+**
+**	Ajouter des éléments de décor tel que:
+**	biscuits en forme de poisson
+**	bonbons en forme de poisson
+**	déchets en forme de poisson
+**	débriss en forme de poisson
+**	de l'éthylbenzène de poisson
+**	des écorces de réglis
+**	composants organiques en forme de poisson
+**	et sédiments en forme de sédiments
+**	beure de cacahuete caramelisé en forme de poisson
+**	un zèste de citron
+**	des résines alpha
+**	de la résine de polyesthère non saturée
+**	des résines a fibre de verre
+**	et des composés volatiles de lait malté
+**	neuf gros jaunes d'oeufs
+**	douze membranes géo-synthétiques moyennes
+**	100g de sucre semoule
+**	une touche de "Comment tuer quelqu'un a main nue ?"
+**	deux verres de dés de rhubarbe
+**	trois verres de rhubarbe en poudre
+**	une cuillerée a soupe de rhubarbe tous-usage
+**	une cuillerée a cafe de rhubarbe rapée
+**	trois cuillerées a soupe de rhubarbe flambée
+**	une grosse rhubarbe
+**	une rhubarbe d'imagerie éléctromagnetique a sondes croisées
+**	deux cuillerées a soupe de jus de rhubarbe
+**	un repose tete ajustable en alluminium
+**	un injecteur électrique a aiguille
+**	un injecteur électrique a aiguille sans fil
+**	un outil d'insèrtion d'aiguille
+**	un pistolet a aiguille
+**	des protèges cranes
+**
+**	Il contiens tous les conservateurs, agents de pénétration, gaz et substances
+**	chimiques autorisées pour prévenir la putréfaction malodorante des tissus.
+*/
 
 #endif
